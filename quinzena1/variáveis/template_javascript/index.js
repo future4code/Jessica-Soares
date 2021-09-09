201821893e5cda887886588988c0bb37665a942c
JessicaSoaresFernandes <jessicasf26011992@gmail.com>
2021-09-08 22:02:23 -0300

    Exercício sobre variáveis

---
+++ quinzena1/variáveis/template_javascript/index.js
@@ -0,0 +1,75 @@
+
+//Exercícios de interpretação de código:
+
+//Respostas:
+
+//Atividade 1.1: Console retorna 10 5
+
+//Atividade 1.2: Console retorna 10 10 10
+
+//Atividade 1.3:
+
+let horasDia = prompt("Quantas horas você trabalha por dia?")
+let salarioDia = prompt("Quanto você recebe por dia?") 
+alert(`Voce recebe ${salarioDia/horasDia} por hora`)
+
+
+//Exercícios de escrita de código
+
+//Exercício 1.2
+
+//Atividades a, b, c:
+
+let nome 
+let idade
+console.log (typeof nome)
+console.log (typeof idade)
+
+//Resposta atividade d: O console retorna "undefined" porque as variáveis não foram definidas.
+
+//Atividade e:
+
+let nome = "Jéssica"
+let idade = 29
+console.log (typeof nome)
+console.log (typeof idade)
+
+//Resposta atividade f: O console retorna "string" e "number".
+
+//Atividade g:
+
+console.log ("Olá,", nome, ", você tem", idade, "anos.")
+
+//Exercício 2.2
+
+const pergunta1 = "Você foi ao trabalho hoje?"
+const pergunta2 = "Você gosta de ouvir música enquanto estuda?"
+const pergunta3 = "Está chovendo?"
+
+const resposta1 = "Sim"
+const resposta2 = "Sim"
+const resposta3 = "Não"
+
+console.log (pergunta1, "-", resposta1)
+console.log (pergunta2, "-", resposta2)
+console.log (pergunta3, "-", resposta3)
+
+//Exercício 2.3
+
+let a = 10
+let b = 25
+let c
+
+c = a
+a = b
+b = c
+
+
+console.log (a, b)
+
+
+
+
+
+
+
