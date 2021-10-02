14d3939bf4f2596e84dcdf2b22cbaa630ba7edd4
JessicaSoaresFernandes <jessicasf26011992@gmail.com>
2021-10-01 23:15:55 -0300

    Projeto quinzena2

---
+++ quinzena2/blackjack/nossoJogo.js
@@ -0,0 +1,27 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+console.log ("Boas vindas ao jogo de Blackjack!")
+
+const confirmarInicioJogo = confirm("Quer iniciar uma nova rodada?")
+
+if (confirmarInicioJogo === true){
+   
+   const carta = comprarCarta
+      console.log(carta.texto)
+      console.log(carta.valor)
+   
+
+}else{
+   console.log("O jogo acabou")
+}
+
