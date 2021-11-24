350827f4a9f2baed34221a8f1d175b636ef69e78
JessicaSoaresFernandes <jessicasf26011992@gmail.com>
2021-11-23 23:16:23 -0300

    Início atividade rend-condicional-form

---
+++ Bloco 6/rend-condicional-form/rend-condicional/src/components/Etapa2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+class Etapa2 extends React.Component {
+
+}
+  
+  export default Etapa2;
---
+++ Bloco 6/rend-condicional-form/rend-condicional/src/components/Final.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+class Final extends React.Component {
+
+}
+  
+  export default Final;
