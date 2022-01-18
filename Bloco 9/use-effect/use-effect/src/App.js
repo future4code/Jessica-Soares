016a19e414ba7c6922944f99b080374f6c1f1222
JessicaSoaresFernandes <jessicasf26011992@gmail.com>
2022-01-17 23:14:17 -0300

    Atividade useEffect finalizada

--- Bloco 9/use-effect/use-effect/src/App.js
+++ Bloco 9/use-effect/use-effect/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PokeCard from "./Components/PokeCard/PokeCard";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, setPokeName] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((res) => {
+        setPokeList(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map((pokemon) => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+      {pokeName && <PokeCard pokemon={pokeName} />}
     </div>
   );
-}
+};
 
 export default App;
