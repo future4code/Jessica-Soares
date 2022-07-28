ebd0474cbd686540647ac7895523762c422a7ebb
JessicaSoaresFernandes <jessicasf26011992@gmail.com>
2022-07-28 20:00:06 -0300

    Início case empresa DogHero

---
+++ Bloco 23/case-dogHero/README.md
@@ -0,0 +1,5 @@
+### Endpoints case Amaro:
+
+1 - Inserir produtos por meio de arquivo json.
+2 - Consultar produto por meio de id.
+3 - Consultar produto pelo nome ou tag. *Não funciona
---
+++ Bloco 23/case-dogHero/src/business/ProductBusiness.ts
@@ -0,0 +1,9 @@
+import { ProductDatabase } from "../data/ProductDatabase";
+import { CustomError } from "../error/customError";
+import { product } from "../model/type";
+
+
+export class ProductBusiness {
+  
+  
+}
